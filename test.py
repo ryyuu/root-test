6449af34fcc8780a449e0233e4dd266772d494d1
ryyuu <wylb7332@gmail.com>
2018-11-29 22:41:12 +0800

    aaaa

---
+++ test.py
@@ -0,0 +1,14 @@
+'''类的继承'''
+class Animal:
+    def eat(self):
+        print('eat')
+    def drink(self):
+        prink('drink')
+    def run(self):
+        print('run')
+    def sleep(self):
+        print('sleep')
+
+wangcai = Animal('旺财')
+wangcai.eat()
+wangcai.drink()
